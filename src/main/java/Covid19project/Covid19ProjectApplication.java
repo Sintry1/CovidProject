d692a9f8408e3b42cb9d802ee92c563ade21f761
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2021-04-27 21:32:12 +0200

    Test push and commit

--- src/main/java/Covid19project/Covid19ProjectApplication.java
+++ src/main/java/Covid19project/Covid19ProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Covid19ProjectApplication {
 
     public static void main(String[] args) {
+        System.out.println("I'm going to fail");
         SpringApplication.run(Covid19ProjectApplication.class, args);
     }
 
